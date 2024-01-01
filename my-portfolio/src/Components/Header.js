d6cb669ff7aff0dbe13204f024f3398ba61688a7
Ritik <ritik@123.com>
2024-01-01 12:13:42 +0530

    header_part_updates

---
+++ my-portfolio/src/Components/Header.js
@@ -0,0 +1,70 @@
+import React from "react";
+import "../App.css";
+import insta from "../photos/001-instagram.png";
+import twitter from "../photos/002-twitter.png";
+import service from "../photos/003-customer-service.png";
+import home from "../photos/004-home.png";
+import info from "../photos/005-info-button.png";
+import phone from "../photos/phone.png";
+import facebook from "../photos/007-facebook.png";
+import linkedin from "../photos/008-linkedin.png";
+import photo from "../photos/my-photo.jpg";
+const Header = () => {
+  return (
+    <>
+      <div className="main-content">
+        <div>
+          <img className="photo" src={photo} alt="img" />
+          <p>Ritik Raj</p>
+          <ul className="connect">
+            <li>
+              <img src={twitter} alt="twitter" />
+            </li>
+            <li>
+              <img src={facebook} alt="facebook" />
+            </li>
+            <li>
+              <img src={insta} alt="instagram" />
+            </li>
+            <li>
+              <img src={insta} alt="Leetcode" />
+            </li>
+            <li>
+              <img src={linkedin} alt="Linkedln" />
+            </li>
+          </ul>
+        </div>
+        <div>
+          <ul className="information">
+            <li>
+              <img src={home} alt="" />
+              Home
+            </li>
+            <li>
+              <img src={info} alt="" />
+              About
+            </li>
+            <li>
+              <img src={info} alt="" />
+              Portfolio
+            </li>
+            <li>
+              <img src={service} alt="" />
+              Services
+            </li>
+            <li>
+              <img src={phone} alt="" />
+              Contact
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <img src="" alt="BIG IMAGE" />
+        <h1>Ritik Raj</h1>
+      </div>
+    </>
+  );
+};
+
+export default Header;
