15f1c56f54e71e206a5375d3a3cfb38adf7bc222
Ritik <ritik@123.com>
2024-01-02 17:43:13 +0530

    Added Styling

--- my-portfolio/src/Components/Header.js
+++ my-portfolio/src/Components/Header.js
@@ -12,56 +12,58 @@ import photo from "../photos/my-photo.jpg";
 const Header = () => {
   return (
     <>
-      <div className="main-content">
-        <div>
-          <img className="photo" src={photo} alt="img" />
-          <p>Ritik Raj</p>
-          <ul className="connect">
-            <li>
-              <img src={twitter} alt="twitter" />
-            </li>
-            <li>
-              <img src={facebook} alt="facebook" />
-            </li>
-            <li>
-              <img src={insta} alt="instagram" />
-            </li>
-            <li>
-              <img src={insta} alt="Leetcode" />
-            </li>
-            <li>
-              <img src={linkedin} alt="Linkedln" />
-            </li>
-          </ul>
+      <div className="box">
+        <div className="main-content">
+          <div className="content">
+            <img className="photo" src={photo} alt="img" />
+            <p>Ritik Raj</p>
+            <ul className="connect">
+              <li>
+                <img src={twitter} alt="twitter" />
+              </li>
+              <li>
+                <img src={facebook} alt="facebook" />
+              </li>
+              <li>
+                <img src={insta} alt="instagram" />
+              </li>
+              <li>
+                <img src={insta} alt="Leetcode" />
+              </li>
+              <li>
+                <img src={linkedin} alt="Linkedln" />
+              </li>
+            </ul>
+          </div>
+          <div className="property">
+            <ul className="information">
+              <li>
+                <img src={home} alt="" />
+                Home
+              </li>
+              <li>
+                <img src={info} alt="" />
+                About
+              </li>
+              <li>
+                <img src={info} alt="" />
+                Portfolio
+              </li>
+              <li>
+                <img src={service} alt="" />
+                Services
+              </li>
+              <li>
+                <img src={phone} alt="" />
+                Contact
+              </li>
+            </ul>
+          </div>
         </div>
         <div>
-          <ul className="information">
-            <li>
-              <img src={home} alt="" />
-              Home
-            </li>
-            <li>
-              <img src={info} alt="" />
-              About
-            </li>
-            <li>
-              <img src={info} alt="" />
-              Portfolio
-            </li>
-            <li>
-              <img src={service} alt="" />
-              Services
-            </li>
-            <li>
-              <img src={phone} alt="" />
-              Contact
-            </li>
-          </ul>
+          <img src="" alt="BIG IMAGE" />
+          <h1>Ritik Raj</h1>
         </div>
-      </div>
-      <div>
-        <img src="" alt="BIG IMAGE" />
-        <h1>Ritik Raj</h1>
       </div>
     </>
   );
